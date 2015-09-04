6f6bf9d5a08dc6bcedfe9fc17b21a68514cfef74
markkoshkin <koshkin_mark@bigmir.net>
2015-09-04 21:56:04 +0300

    Fix

---
+++ MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var context = new SushiTest1Entities();
+            ViewBag.Product = context.Products;
+
+            return View();
+        }
+
+        public ActionResult CategorySort()
+        {
+            var context = new SushiTest1Entities();
+            List<Product> list = context.Products.ToList();
+            List<Product> tempList = new List<Product>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var d = list[i];
+                if (d.CategoryId == 1)
+                {
+                    tempList.Add(d);
+                }
+
+            }
+
+            ViewBag.Rol = tempList;
+            return View();
+        }
+
+        public ActionResult CategorySortSup()
+        {
+            var context = new SushiTest1Entities();
+            List<Product> list = context.Products.ToList();
+            List<Product> tempList = new List<Product>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                var d = list[i];
+                if (d.CategoryId == 4)
+                {
+                    tempList.Add(d);
+                }
+            }
+
+
+
+            ViewBag.Rol = tempList;
+
+            return View();
+        }
+
+    }
+}
--- SishiStudioTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SishiStudioTest.MvcApplication" Language="C#" %>
